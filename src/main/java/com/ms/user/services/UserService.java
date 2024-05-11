f10d0fb5563748677f5b69263782ca203a9217dd
Matheus G. Dias <matheusgiga123@gmail.com>
2024-05-11 14:44:28 -0300

    feat: produce user message after save user

--- src/main/java/com/ms/user/services/UserService.java
+++ src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,13 +9,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserService {
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel) {
-        return this.userRepository.save(userModel);
+        userModel = this.userRepository.save(userModel);
+        this.userProducer.publishMessageEmail(userModel);
+
+        return userModel;
     }
 }
